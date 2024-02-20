3733c4f4287c794eaa2bb198df99fac99395a67c
Anhgelus Morhtuuzh <anhgelus.morhtuuzh@proton.me>
2024-02-20 15:18:00 +0100

    build(gradle): install jda

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     kotlin("jvm") version "1.9.22"
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
+application.mainClass = "${group}.Main"
 group = "world.anhgelus.kordify"
 version = "0.0.1"
+
+val jdaVersion = "5.0.0-beta.20"
 
 repositories {
     mavenCentral()
@@ -11,6 +16,7 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+    implementation("net.dv8tion:JDA:$jdaVersion")
 }
 
 tasks.test {
@@ -18,4 +24,13 @@ tasks.test {
 }
 kotlin {
     jvmToolchain(17)
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.isIncremental = true
+
+    // Set this to the version of java you want to use,
+    // the minimum required for JDA is 1.8
+    sourceCompatibility = "17"
 }
